bd2e5c2ac48d3c882c82ecf3c50db7ad3ef0fc5e
danielkochovski <daniel.kochovski12@gmail>
2023-02-09 15:38:14 +0100

    created the function, placed the arguments, and the concatanation, called it once worked, on to the next 2 times :D

--- homework-js/homework-01/js/appHomework01.js
+++ homework-js/homework-01/js/appHomework01.js
@@ -5,4 +5,11 @@ Write a function named tellFortune that:
 takes 4 arguments: number of children, partner's name, geographic location, job title.
 outputs your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
 Call the function 3 times with 3 different values for the arguments.
-*/ 
+*/
+
+function tellFortune(x, y, z, n) {
+    let fortune = x + y + z + n;
+    console.log("You will be a " + x + " " + "in " + y + " " + "and married to " + z + " " + "with " + n + " " + "kids")
+}
+
+tellFortune("developer", "Puerto Rico", "Sofia Vokhmina", 3);
