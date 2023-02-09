a2605fa27004d7377cd3f9c8abb3b55ff39e9017
danielkochovski <daniel.kochovski12@gmail>
2023-02-09 15:11:53 +0100

    created the homeworkJS folder with empty html and js files

---
+++ homework-js/homework-01/js/appHomework01.js
@@ -0,0 +1,8 @@
+/*
+TASK
+THE FORTUNE TELLER
+Write a function named tellFortune that:
+takes 4 arguments: number of children, partner's name, geographic location, job title.
+outputs your fortune to the screen like so: "You will be a X in Y, and married to Z with N kids."
+Call the function 3 times with 3 different values for the arguments.
+*/ 
