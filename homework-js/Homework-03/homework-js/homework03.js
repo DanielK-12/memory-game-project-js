108bbf7a870157d24de29a1437ca6d081d3daa82
danielkochovski <daniel.kochovski12@gmail>
2023-02-19 19:57:53 +0100

    printed the sum, and printed the equation of the problem in new <li> elements

--- homework-js/Homework-03/homework-js/homework03.js
+++ homework-js/Homework-03/homework-js/homework03.js
@@ -6,8 +6,28 @@
 
 
 let niza = [2, 10, 20, 30];
+let sum = 0;
+let equationFormat = '';
 
-for (i = 0; i < niza.length; i++) {
-  myList.innerHTML += `<li>${niza[i]}`
+for (i = 0; i < niza.length; i += 1) {
+  myList.innerHTML += `<li>${niza[i]}</li>`
+  sum += niza[i];
+
+  if (i < niza.length - 1) {
+    equationFormat += `${niza[i]} + `
+  } else {
+    equationFormat += `${niza[i]} = ${sum}`
+  }
 }
+
+document.getElementById("myList").innerHTML += `<li>${sum}</li>`
+document.getElementById("myList").innerHTML += `<li>The equation is : ${equationFormat}</li>`
+
+
+
+
+
+
+
+
 
