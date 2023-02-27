38c58f9bd4c86a4a4f66299c2381a369360fe9e2
danielkochovski <daniel.kochovski12@gmail>
2023-02-27 20:51:04 +0100

    finished objects lecture and exercises

--- class-exercises/carsExercise/carsJS/cars.js
+++ class-exercises/carsExercise/carsJS/cars.js
@@ -3,7 +3,7 @@
 
 
 const car = {
-  name: "mercedes-benz",
+  model: "mercedes-benz",
   color: "silver",
   year: 1957,
   fuel: "petrol",
@@ -13,9 +13,35 @@ const car = {
     return (this.fuelConsumption * distance) / 100;
   },
   returnInfo: function () {
-    return `This car ${this.name} color ${this.color} is from ${this.year}`
+    return `This car ${this.model} color ${this.color} is from ${this.year}`
   }
 }
-
+let input = prompt()
 console.log(car.returnInfo());
 console.log(`For the distance of 500km you will need  ${car.calcFuelConsumption(500)}L of ${car.fuel}.`);
+
+// function constructor
+function Bike(model, color, year, fuel, fuelConsumption) {
+  this.model = model;
+  this.color = color;
+  this.year = year;
+  this.fuel = fuel;
+  this.fuelConsumption = fuelConsumption;
+
+  // method for fuel consumption
+  this.calcFuelConsumption = function (distance) {
+    return (this.fuelConsumption * distance) / 10;
+  }
+  // method to prin the bike info
+  this.printInfo = function () {
+    return `The bike ${this.model} is from ${this.year} and has a color ${this.color}`;
+  }
+};
+
+let bmw = new Bike('RnineT', 'silver', 2019, 'petrol', 8);
+let suzuki = new Bike('GSR750', 'black', 2016, 'petrol', 6);
+
+console.log(bmw);
+console.log(suzuki);
+console.log(bmw.printInfo());
+console.log(suzuki.printInfo());
