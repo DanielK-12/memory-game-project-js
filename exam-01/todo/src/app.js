16921ed7fc64d2da18fefbd5852fcc402c3915cc
danielkochovski <daniel.kochovski12@gmail>
2023-03-27 16:41:33 +0200

    resolved todo list/counter

--- exam-01/todo/src/app.js
+++ exam-01/todo/src/app.js
@@ -4,17 +4,40 @@
  * Enable the user to check the tasks - increment the done counter
  */
 
-const btnAddTask = document.getElementById('btn-add-task');
-const addTask = document.getElementById('add-task');
+const addTaskBtn = document.getElementById('btn-add-task');
+addTaskBtn.addEventListener('click', addTask);
 
-btnAddTask.addEventListener('click', () => {
-  let renderTodoList = document.getElementById('render-todo-list');
-  renderTodoList.innerHTML += `<input type='checkbox'>${addTask.value}</input><br/>`;
-  addTask.value = "";
-  if (e.target.classList.contains('checkbox')) {
+taskCount = 0;
+doneTasks = 0;
 
+function addTask() {
+  let taskList = document.getElementById('render-todo-list');
+  let newTask = document.getElementById('add-task').value;
+
+  let li = `<li><span>${newTask} <input type="checkbox"></span></li>`;
+  taskList.innerHTML += li;
+  document.getElementById('add-task').value = '';
+  taskCount++;
+  updateCounters();
+
+}
+
+function updateCounters() {
+  document.getElementById('total').innerText = taskCount;
+  document.getElementById('done').innerText = doneTasks;
+  document.getElementById('left').innerText = taskCount - doneTasks;
+}
+
+document.addEventListener('click', function (e) {
+  if (e.target.type === 'checkbox') {
+    if (e.target.checked) {
+      doneTasks++;
+      e.target.parentNode.classList.add('task-done');
+    }
+    else {
+      doneTasks--;
+      e.target.parentNode.classList.remove('task-done');
+    }
+    updateCounters();
   }
 });
-
-
-
