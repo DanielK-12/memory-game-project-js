c0d1f316b1350d8245bf3ce2b9eca15bdbade481
danielkochovski <daniel.kochovski12@gmail>
2023-03-27 19:18:34 +0200

    finished the exam02

--- exam 02 - final/app.js
+++ exam 02 - final/app.js
@@ -70,86 +70,92 @@ function Bookstore(name) {
       index++;
     }
     element.innerHTML = htmlToAdd;
-  }
+    //   if (Product.qty === 0) {
+    //     const buyBtn = document.querySelector(".buy-btn");
+    //     buyBtn.disabled = true;
+    //     const wishBtn = document.querySelector(".wishlist-btn");
+    //     wishBtn.disabled = true;
+    //   }
+    // }
 
-  //remove cart 
-  this.removeFromCart = function (id) {
-    const position = this.products.indexOf(this.shoppingCart[id]);
-    this.products[position].qty++;
-    this.shoppingCart.splice(id, 1);
-    this.listProducts();
-  }
-
-  // print wishlish
-  this.printWishlist = function () {
-    const element = document.getElementById('wishlist');
-    let htmlToAdd = '';
-    let index = 0;
-    for (let product of this.wishList) {
-      htmlToAdd += `<li data-index="${index}" id="${product.id}">${product.title}, ${product.price}<button class="remove-wishlist-item"
-  id="${product.id}">Remove</button> </li>`;
-      index++;
+    //remove cart 
+    this.removeFromCart = function (id) {
+      const position = this.products.indexOf(this.shoppingCart[id]);
+      this.products[position].qty++;
+      this.shoppingCart.splice(id, 1);
+      this.listProducts();
     }
-    element.innerHTML = htmlToAdd;
+
+    // print wishlish
+    this.printWishlist = function () {
+      const element = document.getElementById('wishlist');
+      let htmlToAdd = '';
+      let index = 0;
+      for (let product of this.wishList) {
+        htmlToAdd += `<li data-index="${index}" id="${product.id}">${product.title}, ${product.price}<button class="remove-wishlist-item"
+  id="${product.id}">Remove</button> </li>`;
+        index++;
+      }
+      element.innerHTML = htmlToAdd;
+    }
+    // remove wishlish
+
+    this.removeFromwishList = function (index) {
+      this.wishList.splice(index, 1);
+      const btnWish = document.querySelector(".wishlist-btn");
+      btnWish.disabled = false;
+    }
   }
-  // remove wishlish
 
-  this.removeFromwishList = function (index) {
-    this.wishList.splice(index, 1);
-    const btnWish = document.querySelector(".wishlist-btn");
-    btnWish.disabled = false;
+  function Product(title, author, thumbnail, price, quantity, description) {
+    this.id = Date.now();
+    this.title = title;
+    this.author = author;
+    this.thumbnail = thumbnail;
+    this.price = price;
+    this.qty = quantity;
+    this.desc = description;
   }
-}
 
-function Product(title, author, thumbnail, price, quantity, description) {
-  this.id = Date.now();
-  this.title = title;
-  this.author = author;
-  this.thumbnail = thumbnail;
-  this.price = price;
-  this.qty = quantity;
-  this.desc = description;
-}
+  // instances
+  const myStore = new Bookstore("Daniels Bookstore");
 
-// instances
-const myStore = new Bookstore("Daniels Bookstore");
+  // event listeners 
+  const addBtn = document.getElementById("add-btn");
+  addBtn.addEventListener('click', function (e) {
+    e.preventDefault();
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const thumbnail = document.getElementById("thumbnail").value;
+    const price = document.getElementById("price").value;
+    const quantity = document.getElementById("quantity").value;
+    const description = document.getElementById("description").value;
 
-// event listeners 
-const addBtn = document.getElementById("add-btn");
-addBtn.addEventListener('click', function (e) {
-  e.preventDefault();
-  const title = document.getElementById("title").value;
-  const author = document.getElementById("author").value;
-  const thumbnail = document.getElementById("thumbnail").value;
-  const price = document.getElementById("price").value;
-  const quantity = document.getElementById("quantity").value;
-  const description = document.getElementById("description").value;
-
-  myStore.addProduct(new Product(title, author, thumbnail, price, quantity, description));
-  myStore.listProducts();
-});
+    myStore.addProduct(new Product(title, author, thumbnail, price, quantity, description));
+    myStore.listProducts();
+  });
 
 
-document.addEventListener('click', function (e) {
-  e.preventDefault();
-  if (e.target.classList.contains("buy-btn")) {
-    const id = parseInt(e.target.closest("li").id);
-    myStore.addToCart(id);
-    myStore.printCart();
-  } else if (e.target.classList.contains("wishlist-btn")) {
-    const index = parseInt(e.target.closest("li").id);
-    myStore.addToWishlist(index);
-    myStore.printWishlist();
-    const btnWish = document.querySelector(".wishlist-btn");
-    btnWish.disabled = true;
-  } else if (e.target.classList.contains("remove-shopping-item")) {
-    const index = parseInt(e.target.closest("li").getAttribute("data-index"));
-    myStore.removeFromCart(index);
-    myStore.printCart();
-  } else if (e.target.classList.contains('remove-wishlist-item')) {
-    const indexx = parseInt(e.target.closest("li").getAttribute("data-index"))
-    myStore.removeFromwishList(indexx);
-    myStore.printWishlist();
-  }
-});
+  document.addEventListener('click', function (e) {
+    e.preventDefault();
+    if (e.target.classList.contains("buy-btn")) {
+      const id = parseInt(e.target.closest("li").id);
+      myStore.addToCart(id);
+      myStore.printCart();
+    } else if (e.target.classList.contains("wishlist-btn")) {
+      const index = parseInt(e.target.closest("li").id);
+      myStore.addToWishlist(index);
+      myStore.printWishlist();
+      const btnWish = document.querySelector(".wishlist-btn");
+      btnWish.disabled = true;
+    } else if (e.target.classList.contains("remove-shopping-item")) {
+      const index = parseInt(e.target.closest("li").getAttribute("data-index"));
+      myStore.removeFromCart(index);
+      myStore.printCart();
+    } else if (e.target.classList.contains('remove-wishlist-item')) {
+      const indexx = parseInt(e.target.closest("li").getAttribute("data-index"))
+      myStore.removeFromwishList(indexx);
+      myStore.printWishlist();
+    }
+  });
 
